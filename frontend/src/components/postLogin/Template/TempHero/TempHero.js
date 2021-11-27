1359e79a63204d37b05a7975092dc9d46324f014
wh0sumit <sumikumar1608@gmail.com>
2021-11-27 17:05:52 +0530

    Changes

--- frontend/src/components/postLogin/Template/TempHero/TempHero.js
+++ frontend/src/components/postLogin/Template/TempHero/TempHero.js
@@ -35,7 +35,7 @@ export default function TempHero() {
           /> */}
         <h1 class="display-5 fw-bold">{heroTitle}</h1>
         <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">{heroubTitle}</p>
+          <p class="lead mb-4">{herosubTitle}</p>
           {/* <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
               <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
                 Primary button
